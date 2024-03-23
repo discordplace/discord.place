bb2e1aac531c0acd81905c64909de73f2b17b481
chimpdev <skyhancloud@gmail.com>
2024-03-23 04:54:07 +0300

    Fix eslint configs

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,10 +1,50 @@
 /* eslint-disable linebreak-style */
 
 module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
   globals: {
     client: 'readonly',
     config: 'readonly',
     logger: 'readonly',
     passport: 'readonly'
-  }
+  },
+  extends: 'eslint:recommended',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'windows',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+  },
 };
