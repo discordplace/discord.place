99551815f3d48bfd14c5ca86e0f56f2e1b3c2f4f
chimpdev <skyhancloud@gmail.com>
2024-03-24 05:34:27 +0300

    Remove whitespace

--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 module.exports = function connectDatabase(url) {
   if (!url) throw new Error('Missing database URL.');
 
-  mongoose.connect(url, {  dbName: process.env.NODE_ENV === 'production' ? 'api' : 'development' })
+  mongoose.connect(url, { dbName: process.env.NODE_ENV === 'production' ? 'api' : 'development' })
     .then(() => logger.send('Connected to database.'));
 };
