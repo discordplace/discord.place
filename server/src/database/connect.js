ac671342c455e5ac30448fbbf1323ceae248b44a
chimpdev <skyhancloud@gmail.com>
2024-03-24 05:27:40 +0300

    Update database connection to use different database name based on environment

--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -2,5 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function connectDatabase(url) {
   if (!url) throw new Error('Missing database URL.');
-  mongoose.connect(url, { dbName: 'api' }).then(() => logger.send('Connected to database.'));
+
+  mongoose.connect(url, {  dbName: process.env.NODE_ENV === 'production' ? 'api' : 'development' })
+    .then(() => logger.send('Connected to database.'));
 };
