2a4e0bfd4041283b187d7ac3db2974681afef0ba
chimpdev <skyhancloud@gmail.com>
2024-04-07 00:02:51 +0300

    Add new admin quarantine commands

--- server/src/bot/commands/admin.js
+++ server/src/bot/commands/admin.js
@@ -7,6 +7,7 @@ const Premium = require('@/src/schemas/Premium');
 const crypto = require('node:crypto');
 const Review = require('@/schemas/Server/Review');
 const Quarantine = require('@/schemas/Quarantine');
+const ms = require('ms');
 
 const { S3Client, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const S3 = new S3Client({
@@ -56,9 +57,15 @@ module.exports = {
       .addSubcommand(subcommand => subcommand.setName('create').setDescription('Creates a new quarantine entry.')
         .addStringOption(option => option.setName('type').setDescription('The type of the quarantine entry.').setRequired(true).addChoices(...config.quarantineTypes.map(type => ({ name: type, value: type }))))
         .addStringOption(option => option.setName('value').setDescription('The value of the quarantine entry. (User ID, Server ID, etc.)').setRequired(true))
-        .addStringOption(option => option.setName('restriction').setDescription('The restriction of the quarantine entry.').setRequired(true).addChoices(...Object.keys(config.quarantineRestrictions).map(restriction => ({ name: restriction, value: restriction })))))
+        .addStringOption(option => option.setName('restriction').setDescription('The restriction of the quarantine entry.').setRequired(true).addChoices(...Object.keys(config.quarantineRestrictions).map(restriction => ({ name: restriction, value: restriction }))))
+        .addStringOption(option => option.setName('reason').setDescription('The reason for the quarantine entry.').setRequired(true))
+        .addStringOption(option => option.setName('time').setDescription('Expiration time for the quarantine entry. (Optional, 20m, 6h, 3d, 1w, 1m, 1y)')))
       .addSubcommand(subcommand => subcommand.setName('remove').setDescription('Removes a quarantine entry.')
-        .addStringOption(option => option.setName('entry').setDescription('Select the quarantine to remove.').setRequired(true).setAutocomplete(true))))
+        .addStringOption(option => option.setName('entry').setDescription('Select the quarantine to remove.').setRequired(true).setAutocomplete(true)))
+      .addSubcommand(subcommand => subcommand.setName('list').setDescription('Lists all quarantine entries.'))
+      .addSubcommand(subcommand => subcommand.setName('find').setDescription('Finds a quarantine entry.')
+        .addStringOption(option => option.setName('type').setDescription('The type of the quarantine entry.').setRequired(true).addChoices(...config.quarantineTypes.map(type => ({ name: type, value: type }))))
+        .addStringOption(option => option.setName('value').setDescription('The value of the quarantine entry. (User ID, Server ID, etc.)').setRequired(true))))
 
     .toJSON(),
   execute: async interaction => {
@@ -264,13 +271,22 @@ module.exports = {
         const type = interaction.options.getString('type');
         const value = interaction.options.getString('value');
         const restriction = interaction.options.getString('restriction');
+        const reason = interaction.options.getString('reason');
+        const time = interaction.options.getString('time');
 
         const existingQuarantine = await Quarantine.findOne({ type, restriction, [type === 'USER_ID' ? 'user.id' : 'guild.id']: value });
         if (existingQuarantine) return interaction.followUp({ content: `There is already a quarantine entry with the same values. ID: ${existingQuarantine._id}` });
 
+        const quarantineTime = time ? ms(time) : null;
+        if (time && typeof quarantineTime !== 'number') return interaction.followUp({ content: 'Invalid time.' });
+        if (quarantineTime && quarantineTime > 31556952000) return interaction.followUp({ content: 'The maximum quarantine time is 1 year.' });
+
         const quarantine = new Quarantine({
           type,
-          restriction
+          restriction,
+          reason,
+          created_by: interaction.user.id,
+          expire_at: quarantineTime ? new Date(Date.now() + quarantineTime) : null
         });
 
         if (type === 'USER_ID') quarantine.user = { id: value };
@@ -294,6 +310,100 @@ module.exports = {
         await quarantine.deleteOne();
 
         return interaction.followUp({ content: 'Quarantine removed.' });
+      }
+
+      if (subcommand === 'list') {
+        await interaction.deferReply();
+
+        const quarantines = await Quarantine.find();
+        if (!quarantines.length) return interaction.followUp({ content: 'There are no quarantine entries.' });
+
+        const perPage = 4;
+        const pages = Math.ceil(quarantines.length / perPage);
+        const embeds = [];
+        let currentPage = 1;
+
+        for (let i = 0; i < pages; i++) {
+          const filteredQuarantines = quarantines.slice(i * perPage, (i + 1) * perPage);
+          const formattedQuarantinesText = filteredQuarantines.map(quarantine => `- Quarantine #${quarantine._id}
+ - **Type:** ${quarantine.type}
+ - **Value:** ${quarantine.type === 'USER_ID' ? quarantine.user.id : quarantine.guild.id}
+ - **Restriction:** ${quarantine.restriction}
+ - **Reason:** ${quarantine.reason}
+ - **Created by:** <@${quarantine.created_by}>
+ - **Expires at:** ${quarantine.expire_at ? new Date(quarantine.expire_at).toLocaleString() : 'Never'}`).join('\n\n'); 
+
+          const embed = new Discord.EmbedBuilder()
+            .setAuthor({ name: `Quarantine List | Page ${currentPage} of ${pages}`, iconURL: interaction.guild.iconURL() })
+            .setColor('Random')
+            .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
+            .setTimestamp()
+            .setDescription(`Total Quarantines: ${quarantines.length}
+
+${formattedQuarantinesText}`);
+
+          embeds.push(embed);
+        }
+
+        const components = [
+          new Discord.ActionRowBuilder()
+            .addComponents(
+              new Discord.ButtonBuilder()
+                .setCustomId('previous')
+                .setLabel('Previous Page')
+                .setStyle(Discord.ButtonStyle.Secondary),
+              new Discord.ButtonBuilder()
+                .setCustomId('next')
+                .setLabel('Next Page')
+                .setStyle(Discord.ButtonStyle.Secondary)
+            )
+        ];
+
+        const message = await interaction.followUp({ embeds: [embeds[0]], components, fetchReply: true });
+        const collector = message.createMessageComponentCollector({ componentType: Discord.ComponentType.Button, time: 300000 });
+        
+        collector.on('collect', async buttonInteraction => {
+          if (buttonInteraction.user.id !== interaction.user.id) return buttonInteraction.reply({ content: 'You are not allowed to interact with this button.', ephemeral: true });
+
+          if (buttonInteraction.customId === 'previous') {
+            if (currentPage === 1) return buttonInteraction.reply({ content: 'You are already on the first page.', ephemeral: true });
+
+            currentPage--;
+            await buttonInteraction.update({ embeds: [embeds[currentPage - 1]] });
+          }
+
+          if (buttonInteraction.customId === 'next') {
+            if (currentPage === pages) return buttonInteraction.reply({ content: 'You are already on the last page.', ephemeral: true });
+
+            currentPage++;
+            await buttonInteraction.update({ embeds: [embeds[currentPage - 1]] });
+          }
+        });
+
+        collector.on('end', () => message.edit({ content: 'This message is now inactive.', components: [] }));
+      }
+
+      if (subcommand === 'find') {
+        await interaction.deferReply();
+
+        const type = interaction.options.getString('type');
+        const value = interaction.options.getString('value');
+        const quarantine = await Quarantine.findOne({ type, [type === 'USER_ID' ? 'user.id' : 'guild.id']: value });
+        if (!quarantine) return interaction.followUp({ content: 'Quarantine not found.' });
+
+        const embed = new Discord.EmbedBuilder()
+          .setAuthor({ name: `Quarantine #${quarantine._id}`, iconURL: interaction.guild.iconURL() })
+          .setColor('Random')
+          .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
+          .setTimestamp()
+          .setDescription(`- **Type:** ${quarantine.type}
+- **Value:** ${quarantine.type === 'USER_ID' ? quarantine.user.id : quarantine.guild.id}
+- **Restriction:** ${quarantine.restriction}
+- **Reason:** ${quarantine.reason}
+- **Created by:** <@${quarantine.created_by}>
+- **Expires at:** ${quarantine.expire_at ? new Date(quarantine.expire_at).toLocaleString() : 'Never'}`);
+
+        return interaction.followUp({ embeds: [embed] });
       }
     }
   },
@@ -361,10 +471,7 @@ module.exports = {
 
       if (subcommand === 'remove') {
         const quarantines = await Quarantine.find();
-        return interaction.customRespond(quarantines.map(quarantine => {
-          const value = quarantine.type === 'USER_ID' ? quarantine.user.id : quarantine.guild.id;
-          return { name: `${quarantine._id} | ${quarantine.type}: ${value} | Restriction: ${quarantine.restriction}`, value: quarantine._id };
-        }));
+        return interaction.customRespond(quarantines.map(quarantine => ({ name: quarantine._id, value: quarantine._id })));
       }
     }
   }
