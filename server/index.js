24b05c8d11d65c0c4f287340ed170c0c3b0128b0
chimpdev <skyhancloud@gmail.com>
2024-04-18 02:17:39 +0300

    Add todo

--- server/index.js
+++ server/index.js
@@ -38,6 +38,7 @@ const Server = require('@/src/server.js');
 const server = new Server();
 server.create().start(config.port.backend);
 
+// TODO: change the unknown error message where validateSync is used with the error returned by validateSync
 const connectDatabase = require('@/src/database/connect.js');
 connectDatabase(process.env.MONGO_URL, {
   backup: process.env.NODE_ENV === 'production'
