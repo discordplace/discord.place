326e546b2e9db7af40feb7f54d27f100bc3626d8
chimpdev <skyhancloud@gmail.com>
2024-04-26 10:02:20 +0300

    Fix comparison operator in userUpdate.js

--- server/src/bot/events/userUpdate.js
+++ server/src/bot/events/userUpdate.js
@@ -1,5 +1,5 @@
 module.exports = async (oldUser, newUser) => {
-  if (oldUser.banner !== newUser.banner && client.forceFetchedUsers.has(newUser.id)) {
+  if (oldUser.banner != newUser.banner && client.forceFetchedUsers.has(newUser.id)) {
     await client.users.fetch(newUser.id, { force: true })
       .then(user => logger.send(`User ${user.id} has changed their banner, force fetched user data.`))
       .catch(err => logger.send(`User ${newUser.id} has changed their banner, but an error occurred while force fetching user data:\n${err.stack}`));
