2b4eaa48bc3fc20b0468b1b9ea7fd5035c8ee323
chimpdev <skyhancloud@gmail.com>
2024-05-06 06:03:42 +0300

    Update Pagination component with disableAnimation prop

--- client/app/components/Pagination/index.jsx
+++ client/app/components/Pagination/index.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { CgChevronLeft, CgChevronRight } from 'react-icons/cg';
 import cn from '@/lib/cn';
 
-export default function Pagination({ page, setPage, loading, total, limit }) {
+export default function Pagination({ page, setPage, loading, total, limit, disableAnimation }) {
   
   const totalPages = Math.ceil(total / limit);
   const pagesToShow = 1;
@@ -15,15 +15,29 @@ export default function Pagination({ page, setPage, loading, total, limit }) {
   if (pages[0] !== 1) pages.unshift(1);
   if (pages[pages.length - 1] !== totalPages) pages.push(totalPages);
 
+  let initial;
+  let animate;
+  let exit;
+  
+  if (disableAnimation) {
+    initial = { opacity: 1, y: 0 };
+    animate = { opacity: 1, y: 0 };
+    exit = { opacity: 1, y: 0 };
+  } else {
+    initial = { opacity: 0, y: 50 };
+    animate = { opacity: 1, y: 0 };
+    exit = { opacity: 0, y: 50 };
+  }
+
   return (
     <motion.div 
       className={cn(
         'flex items-center p-2 mt-6 overflow-hidden rounded-lg bg-secondary gap-x-2',
         pages.length === 0 && 'hidden'
       )} 
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 50 }} 
+      initial={initial}
+      animate={animate}
+      exit={exit}
     >
       <button 
         className='select-none items-center w-[28px] h-[28px] flex justify-center text-lg font-semibold rounded bg-quaternary disabled:pointer-events-none dark:hover:text-black dark:hover:bg-white hover:bg-black hover:text-white'
