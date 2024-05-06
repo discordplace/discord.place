5b66e58b6d158de6f599aa31c70a45535569eadc
chimpdev <skyhancloud@gmail.com>
2024-05-06 04:09:34 +0300

    Make responsive grid layout for PopularBots component

--- client/app/(bots)/bots/components/Hero/PopularBots/index.jsx
+++ client/app/(bots)/bots/components/Hero/PopularBots/index.jsx
@@ -42,7 +42,7 @@ export default function PopularBots() {
     ) : (
       <>
         <motion.div 
-          className='grid grid-cols-4 gap-4'
+          className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
         >
