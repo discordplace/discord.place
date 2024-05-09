e72a26cc1330bbb28fe05e8298b98bd5b0e3a2a4
chimpdev <skyhancloud@gmail.com>
2024-05-09 18:50:40 +0300

    Update index calculation logic in Hero component for server search page

--- client/app/(servers)/servers/components/Hero/index.jsx
+++ client/app/(servers)/servers/components/Hero/index.jsx
@@ -216,7 +216,7 @@ export default function Hero() {
                       transition={{ ...sequenceTransition, delay: (index * 0.1) }}
                       className='flex animate-scroll-based-appear'
                     >
-                      <ServerCard server={server} index={index} />
+                      <ServerCard server={server} index={page > 1 ? (index + 1) + ((page - 1) * limit) : index} />
                     </motion.div>
                   ))
                 )}
