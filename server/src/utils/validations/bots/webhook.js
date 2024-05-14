63de9d793c6af6a2a2ce3ad0d250770d6b9877d4
BurakYs <brkyhs0@gmail.com>
2024-05-14 22:57:12 +0300

    .

--- server/src/utils/validations/bots/webhook.js
+++ server/src/utils/validations/bots/webhook.js
@@ -1,18 +1,9 @@
-const { URL } = require('node:url');
+const webhookUrlValidation = require('@/validations/bots/webhook');
 
 function webhookValidation(data) {
   if (!data?.url || !data?.token) throw new Error('Webhook should have url and token properties.');
 
-  try {
-    const url = new URL(data.url);
-
-    if (url.protocol !== 'https:') throw new Error('Invalid protocol. Only HTTPS URLs are allowed.');
-    if (url.hostname.endsWith('discord.place')) throw new Error('Invalid hostname. Please provide a valid HTTPS URL.');
-
-    return true;
-  } catch (error) {
-    throw new Error('Invalid URL format. Please provide a valid HTTPS URL.');
-  }
+  return webhookUrlValidation(data.url);
 }
 
 module.exports = webhookValidation;
