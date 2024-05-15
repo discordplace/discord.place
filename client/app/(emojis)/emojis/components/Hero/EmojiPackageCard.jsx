012eb1c34d8c4646127cdc7125eefaae9788c697
chimpdev <skyhancloud@gmail.com>
2024-05-15 05:17:00 +0300

    Add hover effect to EmojiPackageCard (show all emojis on hover)

--- client/app/(emojis)/emojis/components/Hero/EmojiPackageCard.jsx
+++ client/app/(emojis)/emojis/components/Hero/EmojiPackageCard.jsx
@@ -4,37 +4,49 @@ import { HiDocumentDownload } from 'react-icons/hi';
 import config from '@/config';
 import cn from '@/lib/cn';
 import { LuPackage } from 'react-icons/lu';
+import { useState } from 'react';
 
 export default function EmojiCard({ id, name, categories, downloads, emoji_ids }) {
+  const [containerHovered, setContainerHovered] = useState(false);
+
   return (
     <div className='flex flex-col w-full min-w-[155px]'>
       <Link 
         className='group relative p-2 flex items-center justify-center h-[110px] rounded-t-2xl bg-secondary lg:hover:bg-quaternary transition-colors group overflow-clip w-full' 
         href={`/emojis/packages/${id}`}
+        onMouseEnter={() => setContainerHovered(true)}
+        onMouseLeave={() => setContainerHovered(false)}
       >
         <div className={cn(
-          'flex gap-2 -space-x-6'
+          containerHovered ? 'grid grid-cols-3 gap-1' : 'flex gap-2 -space-x-6'
         )}>
-          {emoji_ids.slice(0, 4).map((packaged_emoji, index) => (
+          {emoji_ids.slice(0, containerHovered ? 9 : 4).map((packaged_emoji, index) => (
             <>
               <MotionImage 
                 key={packaged_emoji.id}
                 src={config.getEmojiURL(`packages/${id}/${packaged_emoji.id}`, packaged_emoji.animated)}
                 alt={`Emoji package ${name} ${index + 1}. emoji`} 
                 className={cn(
-                  'transition-all object-contain  ease-in-out w-[48px] h-[48px] p-0.5 rounded-3xl bg-quaternary'
+                  'transition-all object-contain ease-in-out w-[48px] h-[48px] p-0.5 rounded-3xl bg-quaternary',
+                  containerHovered && 'w-[32px] h-[32px] rounded-md'
                 )}
-                width={64}
-                height={64}
+                width={containerHovered ? 32 : 64}
+                height={containerHovered ? 32 : 64}
                 style={{
                   zIndex: 4 - index
                 }}
               />
             </>
           ))}
+          {containerHovered && new Array(9 - emoji_ids.length).fill(0).map((_, index) => (
+            <div 
+              key={index}
+              className='w-[32px] h-[32px] p-0.5 rounded-md bg-secondary'
+            />
+          ))}
         </div>
 
-        <div className='absolute px-2 text-xs font-semibold uppercase bg-quaternary rounded-full right-2 top-1.5'>
+        <div className='group-hover:bg-secondary transition-all absolute px-2 text-xs font-semibold uppercase bg-quaternary rounded-full right-2 top-1.5'>
           PACK
         </div>
       </Link>
