1f5b3eb021bdf53d9fef6bf9a5909d6d764b0e2f
chimpdev <skyhancloud@gmail.com>
2024-05-23 14:50:51 +0300

    Revert "Add tooltip to disabled tabs on bot and server pages"
    This reverts commit 378996171cc506398cbf4f6d199a7a6a974ca50e.

--- client/app/(servers)/servers/[id]/components/Tabs/index.jsx
+++ client/app/(servers)/servers/[id]/components/Tabs/index.jsx
@@ -1,9 +1,9 @@
+import cn from '@/lib/cn';
 import { useState } from 'react';
 import TopVoters from '@/app/(servers)/servers/[id]/components/Tabs/TopVoters';
 import VoiceActivityGraph from '@/app/(servers)/servers/[id]/components/Tabs/Graph/VoiceActivity';
 import Reviews from '@/app/(servers)/servers/[id]/components/Tabs/Reviews';
-import Tooltip from '@/app/components/Tooltip';
-import TabSwitcher from '@/app/(servers)/servers/[id]/components/Tabs/TabSwitcher';
+import { motion } from 'framer-motion';
 
 export default function Tabs({ server }) {
   const [activeTab, setActiveTab] = useState('reviews');
@@ -17,29 +17,36 @@ export default function Tabs({ server }) {
       label: 'Top Voters',
       id: 'topVoters',
       component: <TopVoters server={server} />,
-      disabled: server.votes <= 0,
-      tooltip: 'No votes on this server yet.'
+      disabled: server.votes <= 0
     },
     {
       label: 'Voice Activity',
       id: 'voiceActivityGraph',
       component: <VoiceActivityGraph server={server} />,
-      disabled: !server.voiceActivity || server.voiceActivity.filter?.(activity => new Date(activity.createdAt) > new Date(Date.now() - 86400000))?.length === 0,
-      tooltip: !server.voiceActivity ? 'Voice activity is disabled on this server.' : 'No voice activity in the last 24 hours.'
+      disabled: !server.voiceActivity || server.voiceActivity.filter?.(activity => new Date(activity.createdAt) > new Date(Date.now() - 86400000))?.length === 0
     }
   ];
-
+  
   return (
     <>
       <div className='flex mx-8 my-8 rounded-full lg:mx-0 w-max bg-tertiary'>
         {tabs.map(tab => (
-          tab.disabled ? (
-            <Tooltip key={tab.id} content={tab.tooltip}>
-              <TabSwitcher tab={tab} activeTab={activeTab} setActiveTab={setActiveTab} />
-            </Tooltip>
-          ) : (
-            <TabSwitcher key={tab.id} tab={tab} activeTab={activeTab} setActiveTab={setActiveTab} />
-          )
+          <div className='relative cursor-pointer select-none group' key={tab.id} onClick={() => !tab.disabled && setActiveTab(tab.id)}>
+            <div className={cn(
+              'px-4 py-2 font-semibold text-sm z-10 relative transition-colors',
+              activeTab === tab.id ? 'text-white dark:text-black duration-500' : 'group-hover:text-tertiary',
+              tab.disabled && 'opacity-50 cursor-not-allowed group-hover:text-primary'
+            )}>
+              {tab.label}
+            </div>
+
+            {activeTab === tab.id && (
+              <motion.div
+                layoutId='tabIndicator'
+                className='absolute bottom-0 left-0 w-full h-full bg-black rounded-full pointer-events-none dark:bg-white'
+              />
+            )}
+          </div>
         ))}
       </div>
         
