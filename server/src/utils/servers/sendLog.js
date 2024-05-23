866a05d9db7a65d258e1214a186b44e1251ae918
chimpdev <skyhancloud@gmail.com>
2024-05-23 21:51:28 +0300

    Add sendLog function for sending log messages to servers

---
+++ server/src/utils/servers/sendLog.js
@@ -0,0 +1,16 @@
+const LogChannel = require('@/schemas/Server/LogChannel');
+
+async function sendLog(guildId, message) {
+  const logChannel = await LogChannel.findOne({ guildId });
+  if (!logChannel) throw new Error(`Log channel not found for guild ${guildId}`);
+
+  const channel = client.channels.cache.get(logChannel.channelId);
+  if (!channel) {
+    await LogChannel.deleteOne({ guildId });
+    throw new Error(`Log channel not found for guild ${guildId}`);
+  }
+
+  return channel.send({ content: `- \`[${new Date().toLocaleString('en-US')}]\` ${message}`, allowedMentions: { parse: [] } });
+}
+
+module.exports = sendLog;
