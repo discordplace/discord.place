79b89f3cce55ae1d510d228953e1ee0257972083
chimpdev <skyhancloud@gmail.com>
2024-06-07 02:05:56 +0300

    Remove cachin behavior for getApproximateGuildCount function

--- server/src/utils/bots/getApproximateGuildCount.js
+++ server/src/utils/bots/getApproximateGuildCount.js
@@ -3,8 +3,6 @@ const axios = require('axios');
 async function getApproximateGuildCount(botId) {
   if (!process.env.DISCORD_BOT_GET_APPROXIMATE_GUILD_COUNT_API_URL) throw new Error('DISCORD_BOT_GET_APPROXIMATE_GUILD_COUNT_API_URL environment variable is missing');
   if (!process.env.DISCORD_BOT_GET_APPROXIMATE_GUILD_COUNT_API_SECRET) throw new Error('DISCORD_BOT_GET_APPROXIMATE_GUILD_COUNT_API_SECRET environment variable is missing');
-
-  if (client.approximateGuildCountFetchedBots.has(botId)) return client.approximateGuildCountFetchedBots.get(botId);
   
   try {
     const response = await axios.get(`${process.env.DISCORD_BOT_GET_APPROXIMATE_GUILD_COUNT_API_URL}/bots/${botId}`, {
@@ -13,17 +11,10 @@ async function getApproximateGuildCount(botId) {
       }
     });
 
-    if (response.status === 200) {
-      client.approximateGuildCountFetchedBots.set(botId, { 
-        approximate_guild_count: response.data.approximate_guild_count, 
-        fetchedAt: response.data.fetchedAt 
-      });
-      
-      return {
-        approximate_guild_count: response.data.approximate_guild_count,
-        fetchedAt: response.data.fetchedAt
-      };
-    }
+    if (response.status === 200) return {
+      approximate_guild_count: response.data.approximate_guild_count,
+      fetchedAt: response.data.fetchedAt
+    };
   } catch (error) {
     logger.send(`There was an error while fetching approximate guild count for bot with ID ${botId}:\n${error.stack}`);
     
