a4ce467ac6f13b85a6d5d9d35f83cec2c1dc9d65
chimpdev <skyhancloud@gmail.com>
2024-06-14 23:25:40 +0300

    Refactor slug route to remove unnecessary caching

--- server/src/routes/[slug].js
+++ server/src/routes/[slug].js
@@ -1,21 +1,5 @@
 const Profile = require('@/schemas/Profile');
 const Premium = require('@/schemas/Premium');
-const urlCache = [];
-
-Profile.watch().on('change', async change => {
-  if (change.operationType === 'delete') {
-    const cachedUrl = urlCache.find(url => url._id.toString() === change.documentKey._id.toString());
-    if (cachedUrl) urlCache.splice(urlCache.indexOf(cachedUrl), 1);
-  }
-
-  if (change.operationType === 'update') {
-    const updatedProfile = await Profile.findById(change.documentKey._id);
-    const cachedUrl = urlCache.find(url => url._id.toString() === change.documentKey._id.toString());
-    if (cachedUrl && !config.customHostnames.includes(updatedProfile.preferredHost)) {
-      urlCache.splice(urlCache.indexOf(cachedUrl), 1);
-    }
-  }
-});
 
 module.exports = {
   get: [
@@ -24,14 +8,7 @@ module.exports = {
         const { slug } = request.params;
         const slugIsValid = /^(?!-)(?!.*--)(?!.*-$)[a-zA-Z0-9-]{3,32}$/.test(slug);
         if (!slugIsValid) return response.redirect(config.frontendUrl + '/error?code=404');
-
-        const cachedUrl = urlCache.find(url => url.slug === slug);
-        if (cachedUrl && Date.now() - cachedUrl.createdTimestamp < 3600000) {
-          return response.redirect(cachedUrl.url);
-        } else if (cachedUrl && Date.now() - cachedUrl.createdTimestamp >= 3600000) {
-          urlCache.splice(urlCache.indexOf(cachedUrl), 1);
-        }
-        
+ 
         const foundProfile = await Profile.findOne({ slug });
         if (!foundProfile) return response.redirect(config.frontendUrl + '/error?code=404');
       
@@ -39,13 +16,7 @@ module.exports = {
         if (!foundPremium) return response.redirect(config.frontendUrl + '/error?code=50002');
         if (!config.customHostnames.includes(foundProfile.preferredHost)) return response.redirect(config.frontendUrl + '/error?code=404');
         if (request.headers.host !== foundProfile.preferredHost) return response.redirect(config.frontendUrl + '/error?code=404');
-          
-        urlCache.push({ 
-          slug, 
-          url: config.frontendUrl + '/profile/' + slug, 
-          createdTimestamp: Date.now(), 
-          _id: foundProfile._id 
-        });
+
         return response.redirect(config.frontendUrl + '/profile/' + slug);
       }
 
