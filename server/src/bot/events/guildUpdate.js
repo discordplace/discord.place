3e845c861152c22827bea4ec0e6502991ed3ca09
chimpdev <skyhancloud@gmail.com>
2024-06-17 21:48:31 +0300

    Delete guildUpdate event listener

--- server/src/bot/events/guildUpdate.js
+++
@@ -1,9 +0,0 @@
-const fetchGuildsMembers = require('@/utils/fetchGuildsMembers');
-
-module.exports = async (oldGuild, newGuild) => {
-  if (!oldGuild.available && newGuild.available) {
-    logger.info(`Guild ${newGuild.name} (${newGuild.id}) became available.`);
-
-    fetchGuildsMembers([newGuild.id]);
-  }
-};
