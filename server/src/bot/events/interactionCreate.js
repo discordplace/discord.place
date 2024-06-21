eb9f63f33c61e698d882a9eb5374c7a374be61f6
chimpdev <skyhancloud@gmail.com>
2024-06-21 19:43:02 +0300

    Refactor interactionCreate event handling for guild-only commands

--- server/src/bot/events/interactionCreate.js
+++ server/src/bot/events/interactionCreate.js
@@ -9,7 +9,7 @@ module.exports = async interaction => {
     const foundCommand = client.commands.find(command => typeof command.data?.toJSON === 'function' ? command.data.toJSON().name === interaction.commandName : command.data.name === interaction.commandName);
     if (!foundCommand) return;
 
-    if (foundCommand.isGuildOnly) return interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
+    if (foundCommand.isGuildOnly && !interaction.guild) return interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
 
     const user = await User.findOneAndUpdate({ id: interaction.user.id }, { id: interaction.user.id }, { upsert: true, new: true });
 
@@ -70,10 +70,10 @@ module.exports = async interaction => {
       );
     };
 
-    if (!interaction.guild) return;
-
     const foundCommand = client.commands.find(command => typeof command.data?.toJSON === 'function' ? command.data.toJSON().name === interaction.commandName : command.data.name === interaction.commandName);
     if (!foundCommand) return;
+
+    if (foundCommand.isGuildOnly && !interaction.guild) return;
 
     try {
       await foundCommand.autocomplete(interaction);
