c2c92b3753345a638282717c825c120f91b4f190
chimpdev <skyhancloud@gmail.com>
2024-06-22 04:16:42 +0300

    Show FullPageLoading component while pages loading

--- client/app/components/Providers/Auth/index.jsx
+++ client/app/components/Providers/Auth/index.jsx
@@ -3,8 +3,10 @@
 import { useEffect } from 'react';
 import useAuthStore from '@/stores/auth';
 import getAuthenticatedUser from '@/lib/request/auth/getAuthenticatedUser';
+import FullPageLoading from '@/app/components/FullPageLoading';
 
 export default function AuthProvider({ children }) {
+  const user = useAuthStore(state => state.user);
   const setUser = useAuthStore(state => state.setUser);
   const setLoggedIn = useAuthStore(state => state.setLoggedIn);
 
@@ -18,6 +20,8 @@ export default function AuthProvider({ children }) {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (user === 'loading') return <FullPageLoading />;
 
   return children;
 }
