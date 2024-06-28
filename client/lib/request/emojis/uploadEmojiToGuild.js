3dad6b8930a1f639590145a988cb6f08e177554b
chimpdev <skyhancloud@gmail.com>
2024-06-28 08:29:58 +0300

    Refactor uploadEmojiToGuild.js to fix packIndex being true always

--- client/lib/request/emojis/uploadEmojiToGuild.js
+++ client/lib/request/emojis/uploadEmojiToGuild.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 export default function uploadEmojiToGuild(id, guildId, packIndex) {
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
-    const url = `${config.api.url}/emojis/${String(packIndex) ? 'packages/' : ''}${id}/upload-to-guild`;
+    const url = `${config.api.url}/emojis/${packIndex !== false ? 'packages/' : ''}${id}/upload-to-guild`;
 
     try {
       await axios.post(url, {
-        packIndex: String(packIndex) || null,
+        packIndex: packIndex !== false ? packIndex : null,
         guildId 
       }, { withCredentials: true });
 
