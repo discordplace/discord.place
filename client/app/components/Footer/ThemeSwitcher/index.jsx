8ac2831e657a12a70cc214cb5bce0ad0621e1b0c
chimpdev <skyhancloud@gmail.com>
2024-06-30 05:17:03 +0300

    Redesign theme switcher

--- client/app/components/Footer/ThemeSwitcher/index.jsx
+++ client/app/components/Footer/ThemeSwitcher/index.jsx
@@ -1,4 +1,82 @@
-import { Drawer } from 'vaul';
+'use client';
+
+import useModalsStore from '@/stores/modals';
+import { useShallow } from 'zustand/react/shallow';
+import { MdSunny } from 'react-icons/md';
+import { IoIosMoon } from 'react-icons/io';
+
+export default function ThemeSwitcher() {
+  const { openModal } = useModalsStore(useShallow(state => ({
+    openModal: state.openModal
+  })));
+
+  return (
+    <button
+      className='w-full px-3 py-2 text-sm font-semibold select-none sm:w-max rounded-xl text-tertiary bg-tertiary hover:text-primary hover:bg-quaternary'
+      onClick={() => 
+        openModal('theme-switcher', {
+          maxWidth: '600px',
+          title: 'Appearance Settings',
+          description: 'Choose your preferred interface appearance. You have the option to pick a theme manually.',
+          content: (
+            <div className='flex w-full my-4 gap-x-4'>
+              <div className='flex flex-col flex-1 w-full h-[280px] border border-primary rounded-lg bg-secondary'>
+                <div className='pl-3 flex items-center gap-x-2 h-[40px] font-medium w-full bg-quaternary rounded-t-lg'>
+                  <MdSunny />
+                  Light Theme
+                </div>
+
+                <div className='flex items-center justify-center flex-1 w-full h-full'>
+                </div>
+
+                <div className='flex flex-col w-full p-3 rounded-b-lg h-[80px] bg-quaternary gap-y-1'>
+                  <h3 className='text-sm font-medium text-secondary'>Light Mode</h3>
+                  <p className='text-xs text-tertiary'>Bright design for well-lit environments.</p>
+                </div>
+              </div>
+
+              <div className='flex flex-col flex-1 w-full h-[280px] border border-primary rounded-lg bg-secondary'>
+                <div className='pl-3 flex items-center gap-x-2 h-[40px] font-medium w-full bg-quaternary rounded-t-lg'>
+                  <IoIosMoon />
+                  Dark Theme
+                </div>
+
+                <div className='flex flex-1 w-full h-full'>
+                  sad
+                </div>
+
+                <div className='flex flex-col w-full p-3 rounded-b-lg h-[80px] bg-quaternary gap-y-1'>
+                  <h3 className='text-sm font-medium text-secondary'>Dark Mode</h3>
+                  <p className='text-xs text-tertiary'>Eye-friendly design for low-light environments.</p>
+                </div>
+              </div>
+            </div>
+          ),
+          buttons: [
+            {
+              id: 'cancel',
+              label: 'Cancel',
+              variant: 'ghost',
+              actionType: 'close'
+            },
+            {
+              id: 'save-changes',
+              label: 'Save Changes',
+              variant: 'solid',
+              action: () => {
+                console.log('Saving changes...');
+              }
+            }
+          ]
+        })
+      }
+    >
+      Theme Switcher
+    </button>
+  );
+}
+
+/*import { Drawer } from 'vaul';
 import { useState } from 'react';
 import ThemePreviewButton from '@/app/components/Footer/ThemeSwitcher/PreviewButton';
 
@@ -33,4 +111,4 @@ export default function ThemeSwitcher() {
       </Drawer.Portal>
     </Drawer.Root>
   );
-}
+}*/
