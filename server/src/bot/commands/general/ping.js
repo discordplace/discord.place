495318cd5a17c690e4c27fb5eac9b19879f3c583
chimpdev <skyhancloud@gmail.com>
2024-07-12 15:14:58 +0300

    Improve ping command execution in ping.js

--- server/src/bot/commands/general/ping.js
+++ server/src/bot/commands/general/ping.js
@@ -3,5 +3,9 @@ module.exports = {
     name: 'ping',
     description: 'Replies with Pong!'
   },
-  execute: interaction => interaction.reply({ content: `Pong! Server latency is ${interaction.client.ws.ping}ms!` })
+  execute: async interaction => {
+    if (!interaction.deferred && !interaction.replied) await interaction.deferReply();
+
+    interaction.followUp({ content: `Pong! Server latency is ${interaction.client.ws.ping}ms!` });
+  }
 };
