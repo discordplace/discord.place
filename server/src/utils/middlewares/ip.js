75b4055cc94e779b6c925bee24cd7f3ec85b9b7b
chimpdev <skyhancloud@gmail.com>
2024-07-17 18:58:17 +0300

    Refactor IP middleware to simplify code and use correct method to get IP adresses

--- server/src/utils/middlewares/ip.js
+++ server/src/utils/middlewares/ip.js
@@ -1,16 +1,6 @@
 module.exports = function (request, response, next) {
-  const headersToCheck = [
-    'x-client-ip',
-    'x-forwarded-for',
-    'x-real-ip',
-    'x-cluster-client-ip',
-    'x-forwarded',
-    'forwarded-for',
-    'forwarded'
-  ];
+  request.clientIp = cleanIp(request.socket.remoteAddress);
 
-  const ip = headersToCheck.reduce((acc, header) => acc || request.headers[header], null) || request.connection.remoteAddress;
-  request.clientIp = cleanIp(ip);
   next();
 
   function cleanIp(ip) {
