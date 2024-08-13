5dace9609d7f59a4828f4706f97c8756a12bba75
chimpdev <skyhancloud@gmail.com>
2024-08-13 07:43:46 +0300

    Remove admin folder

--- admin/src/stores/user.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-export const useUserStore = create(set => ({
-  user: 'initial',
-  setUser: user => set({ user })
-}));
--- admin/src/utils/request/currentUser/index.js
+++
@@ -1,3 +0,0 @@
-import getCurrentUser from '@/utils/request/currentUser/get';
-
-export const get = getCurrentUser;
