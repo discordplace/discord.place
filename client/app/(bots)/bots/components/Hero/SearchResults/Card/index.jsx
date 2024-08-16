a8bf56c6ca8ae4ca0e26e40ee77151784be98214
chimpdev <skyhancloud@gmail.com>
2024-08-16 23:05:13 +0300

    Truncate long text in BotCard component

--- client/app/(bots)/bots/components/Hero/SearchResults/Card/index.jsx
+++ client/app/(bots)/bots/components/Hero/SearchResults/Card/index.jsx
@@ -133,7 +133,7 @@ export default function Card({ data, overridedSort }) {
               {infos.filter(info => info.condition === true).map(info => (
                 <div key={info.icon} className='flex gap-x-1.5 items-center text-sm'>
                   <info.icon className='text-tertiary' />
-                  <span className='text-secondary'>{info.transform ? info.transform(info.value) : formatter.format(info.value)}</span>
+                  <span className='truncate max-w-[115px] text-secondary'>{info.transform ? info.transform(info.value) : formatter.format(info.value)}</span>
                 </div>
               ))}
             </div>
