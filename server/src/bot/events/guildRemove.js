69243b97069a2bb132a6f6780e2a0f1e19431172
chimpdev <skyhancloud@gmail.com>
2024-08-27 20:16:12 +0300

    Refactor guildRemove event to also delete vote rewards for a guild

--- server/src/bot/events/guildRemove.js
+++ server/src/bot/events/guildRemove.js
@@ -1,3 +1,7 @@
 const Review = require('@/schemas/Server/Review');
+const Reward = require('@/schemas/Server/Vote/Reward');
 
-module.exports = guild => Review.deleteMany({ 'server.id': guild.id, approved: false });
+module.exports = guild => Promise.all(
+  Review.deleteMany({ 'server.id': guild.id, approved: false }),
+  Reward.deleteMany({ 'guild.id': guild.id })
+);
