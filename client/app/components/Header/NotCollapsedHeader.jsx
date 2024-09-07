36a38b920928e82d293b50d26c497239c2318b4f
chimpdev <skyhancloud@gmail.com>
2024-09-07 09:36:49 +0300

    Refactor NotCollapsedHeader component to improve header tab functionality

--- client/app/components/Header/NotCollapsedHeader.jsx
+++ client/app/components/Header/NotCollapsedHeader.jsx
@@ -1,64 +1,145 @@
 'use client';
 
-import LogoWithText from '@/app/components/Logo/WithText';
-import config from '@/config';
-import MotionLink from '@/app/components/Motion/Link';
-import UserSide from './UserSide';
 import cn from '@/lib/cn';
-import { motion } from 'framer-motion';
-import { Suspense } from 'react';
-import useLanguageStore, { t } from '@/stores/language';
+import { Suspense, useEffect } from 'react';
+import { FiChevronDown } from 'react-icons/fi';
+import ServicesDropdown from '@/app/components/Header/ServicesDropdown';
+import UserSide from './UserSide';
+import Link from 'next/link';
+import useGeneralStore from '@/stores/general';
+import LogoWithText from '@/app/components/Logo/WithText';
+import { t } from '@/stores/language';
+import { MdMiscellaneousServices } from 'react-icons/md';
+import { BsStars } from 'react-icons/bs';
+import { HiNewspaper } from 'react-icons/hi';
 
-export default function NotCollapsedHeader({ pathname }) {
-  const language = useLanguageStore(state => state.language);
+export default function NotCollapsedHeader() {
+  const hoveringHeaderTab = useGeneralStore(state => state.header.hoveringHeaderTab);
+  const setHoveringHeaderTab = useGeneralStore(state => state.header.setHoveringHeaderTab);
+  const lastMouseOut = useGeneralStore(state => state.header.lastMouseOut);
+  const setLastMouseOut = useGeneralStore(state => state.header.setLastMouseOut);
+
+  const links = [
+    {
+      id: 'services',
+      icon: MdMiscellaneousServices,
+      name: t('header.links.0')
+    },
+    {
+      id: 'blog',
+      icon: HiNewspaper,
+      name: t('header.links.1'),
+      href: '/blogs'
+    },
+    {
+      id: 'premium',
+      icon: BsStars,
+      name: t('header.links.2'),
+      href: '/premium'
+    }
+  ];
+
+  useEffect(() => {
+    if (!hoveringHeaderTab) return;
+
+    const hoveringHeaderTabElement = document.getElementById('hoveringHeaderTab');
+    const headerTabElement = document.getElementById(`headerTab-${hoveringHeaderTab}`);
+    const headerTabsElement = document.getElementById('headerTabs');
+
+    const headerTabElementRect = headerTabElement.getBoundingClientRect();
+    const headerTabsElementRect = headerTabsElement.getBoundingClientRect();
+
+    if (Date.now() - lastMouseOut > 500) {
+      hoveringHeaderTabElement.style.transition = 'opacity 150ms, width 0ms, height 0ms, top 0ms, left 0ms';
+
+      setTimeout(() => {
+        hoveringHeaderTabElement.style.transition = 'all 150ms';
+      }, 150);
+    }
+
+    hoveringHeaderTabElement.style.width = `${headerTabElementRect.width}px`;
+    hoveringHeaderTabElement.style.height = `${headerTabElementRect.height}px`;
+
+    hoveringHeaderTabElement.style.top = `${headerTabElementRect.top - headerTabsElementRect.top}px`;
+    hoveringHeaderTabElement.style.left = `${headerTabElementRect.left - headerTabsElementRect.left}px`;
+  }, [hoveringHeaderTab, lastMouseOut]);
 
   return (
-    <div className="absolute flex items-center justify-center w-full z-[9998] top-0">
-      <div className={cn(
-        'flex justify-between items-center max-w-[1000px] w-full mt-12',
-        language === 'tr' && 'max-w-[1025px]'
-      )}>
-        <div className='flex items-center gap-x-8'>
-          <motion.div
-            className='transition-colors hover:opacity-70'
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.25, delay: 0.15, type: 'spring', stiffness: 100, damping: 20 }}
-          >
+    <div className="absolute flex items-center justify-center top-0 left-0 w-full z-[9999]">
+      <div className="flex items-center justify-between w-full max-w-5xl mt-8">
+        <div className='flex items-center gap-x-12'>
+          <Link href='/'>
             <LogoWithText />
-          </motion.div>
-          
-          <div className='w-[1px] h-[50%] bg-quaternary'>&thinsp;</div>
+          </Link>
 
-          <div className='flex gap-x-6'>
-            {config.headerLinks.map(headerLink => (
-              <MotionLink
-                href={headerLink.href}
-                key={headerLink.href}
-                className={cn(
-                  'flex items-center font-medium transition-colors duration-200 outline-none select-none gap-x-1.5 text-tertiary hover:text-secondary',
-                  pathname === headerLink.href && 'text-primary pointer-events-none'
-                )}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.25, delay: 0.15 * config.headerLinks.indexOf(headerLink), type: 'spring', stiffness: 100, damping: 20 }}
+          <div className='relative flex gap-x-2' id='headerTabs'>
+            {links.map(link => (
+              <Link
+                key={link.name}
+                href={link.href || '#'}
+                className='relative flex items-start justify-center'
+                onMouseEnter={() => setHoveringHeaderTab(link.name)}
+                onMouseLeave={() => {
+                  setLastMouseOut(Date.now());
+                  setHoveringHeaderTab('');
+                }}
               >
-                <headerLink.icon />
-                {t(`header.${headerLink.id}`)}
-              </MotionLink>
+                <div
+                  className='relative z-[2] flex items-center px-3 py-1 text-sm font-semibold transition-all cursor-pointer group gap-x-2 text-tertiary hover:text-primary'
+                  id={`headerTab-${link.name}`}
+                >
+                  <link.icon />
+
+                  {link.name}
+
+                  {link.id === 'services' && (
+                    <FiChevronDown
+                      size={14}
+                      className={cn(
+                        'transition-transform transform',
+                        hoveringHeaderTab === link.name && '-rotate-180'
+                      )}
+                    />
+                  )}
+                </div>
+
+                {link.id === 'services' && (
+                  <div
+                    className={cn(
+                      'absolute pt-12 pb-4 pl-4 pr-4 z-[1]',
+                      hoveringHeaderTab !== link.name && 'pointer-events-none'
+                    )}
+                  >
+                    <div
+                      className={cn(
+                        'transition-all drop-shadow-lg w-max h-max bg-secondary border-2 relative border-primary p-2 rounded-xl',
+                        hoveringHeaderTab === link.name ? 'opacity-100' : 'opacity-0 scale-90 -translate-y-2'
+                      )}
+                    >
+                      <div className='absolute top-0 left-0 flex justify-center w-full h-full'>
+                        <div className='w-3 h-3 rounded-t-[2px] rounded-l-[2px] transform rotate-45 -translate-y-2 border-t-2 border-l-2 border-primary bg-secondary' />
+                      </div>
+
+                      <ServicesDropdown />
+                    </div>
+                  </div>
+                )}
+              </Link>
             ))}
+
+            <div
+              className={cn(
+                'transition-all absolute top-0 left-0 w-full h-full rounded-full z-[-1] bg-tertiary',
+                hoveringHeaderTab ? 'opacity-100' : 'opacity-0'
+              )}
+              id='hoveringHeaderTab'
+            />
           </div>
         </div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.25, type: 'spring', stiffness: 100, damping: 20 }}
-        >
-          <Suspense fallback={<></>}>
-            <UserSide />
-          </Suspense>
-        </motion.div>
+        
+        <Suspense fallback={<></>}>
+          <UserSide />
+        </Suspense>
       </div>
     </div>
   );
---
+++ client/app/components/Header/ServicesDropdown.jsx
@@ -0,0 +1,81 @@
+'use client';
+
+import useGeneralStore from '@/stores/general';
+import { t } from '@/stores/language';
+import Link from 'next/link';
+import { FaCompass } from 'react-icons/fa';
+import { HiTemplate } from 'react-icons/hi';
+import { HiUserGroup } from 'react-icons/hi2';
+import { MdEmojiEmotions } from 'react-icons/md';
+import { PiWaveformBold } from 'react-icons/pi';
+import { RiRobot2Fill } from 'react-icons/ri';
+
+export default function ServicesDropdown() {
+  const setHoveringHeaderTab = useGeneralStore(state => state.header.setHoveringHeaderTab);
+  const setLastMouseOut = useGeneralStore(state => state.header.setLastMouseOut);
+
+  const links = [
+    {
+      name: t('header.servicesDropdownLinks.0'),
+      description: t('profilesPage.subtitle', { count: 0 }),
+      href: '/profiles',
+      icon: HiUserGroup
+    },
+    {
+      name: t('header.servicesDropdownLinks.1'),
+      description: t('serversPage.subtitle'),
+      href: '/servers',
+      icon: FaCompass
+    },
+    {
+      name: t('header.servicesDropdownLinks.2'),
+      description: t('botsPage.subtitle'),
+      href: '/bots',
+      icon: RiRobot2Fill
+    },
+    {
+      name: t('header.servicesDropdownLinks.3'),
+      description: t('emojisPage.subtitle', { count: 0 }),
+      href: '/emojis',
+      icon: MdEmojiEmotions
+    },
+    {
+      name: t('header.servicesDropdownLinks.4'),
+      description: t('templatesPage.subtitle'),
+      href: '/templates',
+      icon: HiTemplate
+    },
+    {
+      name: t('header.servicesDropdownLinks.5'),
+      description: t('soundsPage.subtitle'),
+      href: '/sounds',
+      icon: PiWaveformBold
+    }
+  ];
+
+  return (
+    <div className="grid grid-cols-2 max-w-[500px] h-max relative">
+      {links.map((link, index) => (
+        <Link
+          key={index}
+          href={link.href}
+          onClick={() => {
+            setHoveringHeaderTab('');
+            setLastMouseOut(Date.now());
+          }}
+        >
+          <div className="flex items-center px-4 py-2 cursor-pointer gap-x-3 group hover:bg-tertiary rounded-xl">
+            <div className="flex items-center justify-center flex-shrink-0 rounded-lg w-[40px] h-[40px] text-lg text-primary bg-quaternary">
+              <link.icon className='transition-all group-hover:scale-110' />
+            </div>
+
+            <div className="flex flex-col">
+              <h1 className="text-sm font-semibold text-secondary group-hover:text-primary">{link.name}</h1>
+              <p className="overflow-hidden text-xs font-medium whitespace-pre-wrap group-hover:text-secondary text-tertiary line-clamp-2">{link.description}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
