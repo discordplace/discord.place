08ef2c626f0b39604ddf38412bab6482d9b99970
chimpdev <skyhancloud@gmail.com>
2024-09-11 02:02:12 +0300

    Add error handling to getImageBuffer function

--- client/lib/getImageBuffer.js
+++ client/lib/getImageBuffer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default function getImageBuffer(url) {
-  return axios.get(url, { responseType: 'arraybuffer' }).then(response => response.data);
+  return axios.get(url, { responseType: 'arraybuffer' }).then(response => response.data).catch(() => null);
 }
