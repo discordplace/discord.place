fe105b57e9f6206219dbcd4e36651645eecd9698
chimpdev <skyhancloud@gmail.com>
2024-09-11 05:00:43 +0300

    Update user email value to display 'N/A' if not available

--- client/app/(dashboard)/dashboard/page.js
+++ client/app/(dashboard)/dashboard/page.js
@@ -124,7 +124,7 @@ export default function Page() {
                 },
                 {
                   type: 'email',
-                  value: user.email
+                  value: user.email || 'N/A'
                 },
                 {
                   type: 'date',
