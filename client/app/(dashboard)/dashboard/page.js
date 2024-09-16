ee8b7caeadf8bcf7fb05cbfdc739b096ee81f24d
chimpdev <skyhancloud@gmail.com>
2024-09-16 22:50:04 +0300

    Delete unnecessary whitespaces

--- client/app/(dashboard)/dashboard/page.js
+++ client/app/(dashboard)/dashboard/page.js
@@ -1550,7 +1550,7 @@ export default function Page() {
                   icon: IoMdCheckmarkCircle,
                   action: () => bulkAction({
                     data: data.blockedIps,
-                    action: item => deleteBlockedIP(item.value  ),
+                    action: item => deleteBlockedIP(item.value),
                     fetchKey: 'blockedips'
                   }),
                   hide: !data.permissions?.canDeleteBlockedIps
