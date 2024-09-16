8f75eff6a42bfef1bbf527474bbc6215d3a777bf
chimpdev <skyhancloud@gmail.com>
2024-09-16 22:36:12 +0300

    Refactor handling of errors in getImageBuffer function

--- client/lib/getImageBuffer.js
+++ client/lib/getImageBuffer.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default function getImageBuffer(url) {
-  return axios.get(url, { responseType: 'arraybuffer' }).then(response => response.data).catch(() => null);
+  return axios.get(url, { responseType: 'arraybuffer' }).then(response => response.data).catch(error => {
+    console.error(`Failed to fetch image buffer from ${url}:`, error);
+    return null;
+  });
 }
