b697a37592b8163dbbfcf89a29456d55c1bc975c
chimpdev <skyhancloud@gmail.com>
2024-09-22 17:27:52 +0300

    Update admin refresh command to remove types

--- server/src/bot/commands/admin/refresh.js
+++ server/src/bot/commands/admin/refresh.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const UserHashes = require('@/schemas/User/Hashes');
-const ServerHashes = require('@/schemas/Server/Hashes');
 
 module.exports = {
   data: new Discord.SlashCommandBuilder()
@@ -8,35 +7,24 @@ module.exports = {
     .setDescription('Refreshes the cached hashes of a user/guild.')
 
     .addSubcommand(subcommand => subcommand.setName('hashes').setDescription('Refreshes the cached hashes of a user/guild.')
-      .addStringOption(option => option.setName('type').setDescription('The type of the cache.').setRequired(true).addChoices({ name: 'User', value: 'user' }, { name: 'Guild', value: 'guild' }))
       .addStringOption(option => option.setName('id').setDescription('The ID of the user/guild.').setRequired(true)))
 
     .toJSON(),
   execute: async interaction => {
     if (!config.permissions.canExecuteRefreshCacheRoles.some(role => interaction.member.roles.cache.has(role))) return interaction.reply({ content: 'You are not allowed to use this command.' });
 
-    const type = interaction.options.getString('type');
     const id = interaction.options.getString('id');
 
-    await interaction.reply({ content: `Updating the cached hashes of ${type === 'user' ? 'User' : 'Guild'} with **ID ${id}**...` });
+    await interaction.reply({ content: `Updating the cached hashes of user **${id}**..` });
     
     try {
-      switch (type) {
-        case 'user': 
-          var userHashes = (await UserHashes.findOne({ id })) || new UserHashes({ id });
-          await userHashes.getNewHashes();
-          
-          break;
-        case 'guild':
-          var serverHashes = (await ServerHashes.findOne({ id })) || new ServerHashes({ id });
-          await serverHashes.getNewHashes();
+      const userHashes = (await UserHashes.findOne({ id })) || new UserHashes({ id });
+      await userHashes.getNewHashes();
 
-          break;
-      }
-      
-      return interaction.editReply({ content: `${type === 'user' ? 'User' : 'Guild'} with **ID ${id}**'s cached hashes has been updated.` });
+      return interaction.editReply({ content: `Successfully updated the cached hashes of user **${id}**.` });
     } catch (error) {
-      return interaction.editReply({ content: `An error occurred while fetching ${type === 'user' ? 'User' : 'Guild'} with **ID ${id}**.` });
+      logger.error(`An error occurred while updating the cached hashes of user ${id}:`, error);
+      return interaction.editReply({ content: `An error occurred while updating the cached hashes of user **${id}**.` });
     }
   }
 };
