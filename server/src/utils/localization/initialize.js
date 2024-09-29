0a1e8092024fc081a5dd82ffad40d27b5c86a750
chimpdev <skyhancloud@gmail.com>
2024-09-30 00:52:21 +0300

    Refactor localization setup and add Discord.js prototype methods

--- server/src/utils/localization/initialize.js
+++ server/src/utils/localization/initialize.js
@@ -1,96 +1,103 @@
 const Discord = require('discord.js');
-const i18n = require('i18next');
+const i18next = require('i18next');
 const intervalPlural = require('i18next-intervalplural-postprocessor');
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 const ServerLanguage = require('@/schemas/Server/Language');
 const moment = require('moment');
 const translate = require('@/utils/localization/translate');
 
-function initialize() {
-  // Get all stored server languages and add them to the cache
-  ServerLanguage.find()
-    .then(languages => languages.forEach(language => this.client.languageCache.set(language.id, language.language)));
+async function initialize() {
+  try {
+    // Get all stored server languages and add them to the cache
+    const languages = await ServerLanguage.find();
+    languages.forEach(language => this.client.languageCache.set(language.id, language.language));
 
-  logger.info('Client created.');
+    logger.info('Client created.');
 
-  // Localization Setup for moment
-  if (!config.availableLocales.find(locale => locale.default)) {
-    logger.error('Default locale is not found in the available locales. Please set the default locale in the config file.');
-    process.exit(1);
-  }
-
-  moment.locale(config.availableLocales.find(locale => locale.default).code);
-
-  // i18n Setup
-  i18n
-    .use(intervalPlural)
-    .init({
-      fallbackLng: config.availableLocales.find(locale => locale.default).code,
-      postProcess: [],
-      interpolation: {
-        escapeValue: false,
-        prefix: '{',
-        suffix: '}'
-      }
-    });
-
-  global.i18n = i18n;
-
-  config.availableLocales.forEach(locale => {
-    const localePath = path.join(__dirname, `../../locales/${locale.code}.json`);
-    const localeContent = fs.readFileSync(localePath, 'utf8');
-
-    i18n.addResourceBundle(locale.code, 'translation', JSON.parse(localeContent), true, true);
-
-    if (!Object.values(Discord.Locale).some(localeCode => localeCode === locale.code)) logger.warn(`Locale ${locale.code} is not supported by Discord.`);
-  });
-
-  // Add a translate method to the Discord.js base interaction object.
-
-  Discord.BaseInteraction.prototype.translate = async function(key, variables = {}) {
-    var language = this.locale;
-    
-    if (this.guildId) {
-      const cachedLanguage = client.languageCache.get(this.guildId);
-      if (cachedLanguage) language = cachedLanguage;
-      else {
-        const serverLanguageData = await ServerLanguage.findOne({ id: this.guildId }).select('language').lean();
-        if (serverLanguageData) language = serverLanguageData.language;
-      }
-    } else {
-      const defaultLanguage = config.availableLocales.find(locale => locale.default).code;
-
-      if (!Object.values(Discord.Locale).some(localeCode => localeCode === this.locale.split('-')[0])) language = defaultLanguage;
-
-      language = this.locale.split('-')[0];
+    const defaultLocale = config.availableLocales.find(locale => locale.default);
+    if (!defaultLocale) {
+      throw new Error('Default locale is not found in the available locales. Please set the default locale in the config file.');
     }
 
+    // Localization Setup for moment
+    moment.locale(defaultLocale.code);
+
+    // i18next Setup
+    await i18next
+      .use(intervalPlural)
+      .init({
+        fallbackLng: defaultLocale.code,
+        interpolation: {
+          escapeValue: false,
+          prefix: '{',
+          suffix: '}'
+        }
+      });
+
+    global.i18n = i18next;
+
+    await Promise.all(config.availableLocales.map(async locale => {
+      const localePath = path.join(__dirname, `../../locales/${locale.code}.json`);
+      const localeContent = await fs.readFile(localePath, 'utf8');
+
+      i18next.addResourceBundle(locale.code, 'translation', JSON.parse(localeContent), true, true);
+
+      if (!Object.values(Discord.Locale).includes(locale.code)) {
+        logger.warn(`Locale ${locale.code} is not supported by Discord.`);
+      }
+    }));
+
+    // Add methods to Discord.js prototypes
+    extendDiscordPrototypes();
+
+  } catch (error) {
+    logger.error('Initialization failed:', error);
+    process.exit(1);
+  }
+}
+
+function extendDiscordPrototypes() {
+  Discord.BaseInteraction.prototype.translate = async function(key, variables = {}) {
+    const language = await this.getLanguage();
     return translate(key, variables, language);
   };
 
-  // Add a getLanguage method to the Discord.js base interaction object.
-
   Discord.BaseInteraction.prototype.getLanguage = async function() {
-    var language = this.locale;
-    
     if (this.guildId) {
-      const cachedLanguage = client.languageCache.get(this.guildId);
-      if (cachedLanguage) language = cachedLanguage;
-      else {
-        const serverLanguageData = await ServerLanguage.findOne({ id: this.guildId }).select('language').lean();
-        if (serverLanguageData) language = serverLanguageData.language;
-      }
-    } else {
-      const defaultLanguage = config.availableLocales.find(locale => locale.default).code;
-
-      if (!Object.values(Discord.Locale).some(localeCode => localeCode === this.locale.split('-')[0])) language = defaultLanguage;
-
-      language = this.locale.split('-')[0];
+      return getGuildLanguage(this.guildId);
     }
-
-    return language;
+    return getDefaultLanguage(this.locale);
   };
+
+  Discord.BaseGuild.prototype.translate = async function(key, variables = {}) {
+    const language = await this.getLanguage();
+    return translate(key, variables, language);
+  };
+
+  Discord.BaseGuild.prototype.getLanguage = async function() {
+    return getGuildLanguage(this.id);
+  };
+}
+
+async function getGuildLanguage(guildId) {
+  let language = client.languageCache.get(guildId);
+  if (!language) {
+    const serverLanguageData = await ServerLanguage.findOne({ id: guildId }).select('language').lean();
+    language = serverLanguageData ? serverLanguageData.language : getDefaultLanguage();
+    client.languageCache.set(guildId, language);
+  }
+  
+  return language;
+}
+
+function getDefaultLanguage(userLocale = '') {
+  const defaultLocale = config.availableLocales.find(locale => locale.default);
+  if (userLocale && Object.values(Discord.Locale).includes(userLocale.split('-')[0])) {
+    return userLocale.split('-')[0];
+  }
+
+  return defaultLocale.code;
 }
 
 module.exports = initialize;
