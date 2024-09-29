cb0bc04528ae6d1260fd3d7d697f8bcf3bfc9023
chimpdev <skyhancloud@gmail.com>
2024-09-30 00:17:51 +0300

    Update guildDelete event to delete reviews and rewards on guild removal

--- server/src/bot/events/guildDelete.js
+++ server/src/bot/events/guildDelete.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
 const updateClientActivity = require('@/utils/updateClientActivity');
-
+const Review = require('@/schemas/Server/Review');
+const Reward = require('@/schemas/Server/Vote/Reward');
 module.exports = async guild => {
   logger.info(`Kicked from guild ${guild.name} (${guild.id}).`);
+
+  Promise.all(
+    Review.deleteMany({ 'server.id': guild.id, approved: false }),
+    Reward.deleteMany({ 'guild.id': guild.id })
+  );
 
   updateClientActivity();
 
--- server/src/bot/events/guildRemove.js
+++
@@ -1,7 +0,0 @@
-const Review = require('@/schemas/Server/Review');
-const Reward = require('@/schemas/Server/Vote/Reward');
-
-module.exports = guild => Promise.all(
-  Review.deleteMany({ 'server.id': guild.id, approved: false }),
-  Reward.deleteMany({ 'guild.id': guild.id })
-);
