67be0d779b0b7c2766fe4a014b70a581e7a49734
chimpdev <skyhancloud@gmail.com>
2024-10-05 07:41:55 +0300

    Update Table component styling for tab count display

--- client/app/(dashboard)/components/Table/index.jsx
+++ client/app/(dashboard)/components/Table/index.jsx
@@ -109,7 +109,7 @@ export default function Table({ tabs }) {
           >
             {tab.label}
 
-            <span className='flex items-center justify-center w-4 h-4 text-xs rounded-full bg-quaternary sm:bg-quaternary text-primary'>
+            <span className='flex items-center justify-center w-max px-1.5 h-4 text-xs rounded-full bg-quaternary sm:bg-quaternary text-primary'>
               {tab.count}
             </span>
 
