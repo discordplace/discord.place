984fd8142f3a347381750935b52cb05454c2b73e
chimpdev <skyhancloud@gmail.com>
2024-10-12 09:18:26 +0300

    Refactor Swagger documentation for root endpoint

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,16 +3,15 @@ module.exports = {
    * @swagger
    * /:
    *  get:
-   *   summary: Responds with OK if server is up and running.
-   *   description: Responds with OK if server is up and running.
-   *   responses:
-   *     200:
-   *      description: OK
-   *      content:
-   *      text/plain:
-   *        schema:
-   *         type: string
-   *         example: OK
+   *    summary: Responds with OK if server is up and running.
+   *    responses:
+   *      200:
+   *        description: OK
+   *        content:
+   *          text/plain:
+   *            schema:
+   *              type: string
+   *              example: OK
    */
   get: (request, response) => {
     const host = request.headers.host;
