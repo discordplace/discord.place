2e385a290cd64a5fc07c2468e2ba9a092663321d
chimpdev <skyhancloud@gmail.com>
2024-10-12 10:12:36 +0300

    Remove Swagger integration

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,18 +1,4 @@
 module.exports = {
-  /**
-   * @swagger
-   * /:
-   *  get:
-   *    summary: Responds with OK if server is up and running.
-   *    responses:
-   *      200:
-   *        description: OK
-   *        content:
-   *          text/plain:
-   *            schema:
-   *              type: string
-   *              example: OK
-   */
   get: (request, response) => {
     const host = request.headers.host;
     if (config.customHostnames.includes(host)) return response.redirect('https://discord.place/profiles');
--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { router } = require('express-file-routing');
 const path = require('node:path');
 const mongoose = require('mongoose');
-const swaggerJsdoc = require('swagger-jsdoc');
 
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -34,7 +33,6 @@ module.exports = class Server {
     while (mongoose.connection.readyState !== mongoose.STATES.connected) await sleep(1000);
 
     this.addMiddlewares();
-    this.setupSwagger();
 
     this.server.use('/', await router({ directory: path.join(__dirname, 'routes') }));
     this.server.use('/public', express.static(path.join(__dirname, '..', 'public')));
@@ -91,33 +89,5 @@ module.exports = class Server {
     if (process.env.NODE_ENV === 'production') this.server.use(blockSimultaneousRequests);
 
     logger.info('Middlewares added.');
-  }
-
-  setupSwagger() {
-    const swaggerOptions = {
-      definition: {
-        openapi: '3.0.0',
-        info: {
-          title: 'discord.place API',
-          version: '1.0.0',
-          description: 'discord.place API Documentation',
-          contact: {
-            name: 'discord.place',
-            url: 'https://discord.place',
-            email: 'support@discord.place'
-          }
-        },
-        servers: [
-          {
-            url: 'https://api.discord.place'
-          }
-        ]
-      },
-      apis: ['./src/routes/*.js']
-    };
-
-    const swaggerSpec = swaggerJsdoc(swaggerOptions);
-    
-    this.server.get('/swagger.json', (request, response) => response.json(swaggerSpec));
   }
 };
