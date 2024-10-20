c31b45ecfadf1cb6c3bfe71425eb94bc31c3a099
chimpdev <skyhancloud@gmail.com>
2024-10-21 02:38:48 +0300

    Update CodeBlock component to support light and dark themes based on user preference

--- docs/components/code-block.jsx
+++ docs/components/code-block.jsx
@@ -1,15 +1,20 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import CopyCodeButton from '@/components/copy-code-button';
 import cn from '@/utils/cn';
+import { useMedia } from 'react-use';
 
 export default function CodeBlock({ children, FileIcon, fileName, language, dimmed }) {
+  const prefersDark = useMedia('(prefers-color-scheme: dark)', false);
+
   return (
     <div className='border border-primary rounded-xl'>
-      <div className={cn(
-        'flex justify-between w-full px-4 py-2 -mb-2 border-b font-geist border-b-primary bg-secondary rounded-t-xl',
-        dimmed && 'bg-[rgba(var(--bg-quaternary))]'
-      )}>
+      <div
+        className={cn(
+          'flex justify-between w-full px-4 py-2 -mb-2 border-b font-geist border-b-primary bg-secondary rounded-t-xl',
+          dimmed && 'bg-[rgba(var(--bg-quaternary))]'
+        )}
+      >
         <div className='text-xs flex items-center gap-x-1.5 text-tertiary'>
           <FileIcon />
 
@@ -24,7 +29,7 @@ export default function CodeBlock({ children, FileIcon, fileName, language, dimm
         // eslint-disable-next-line react/no-children-prop
         children={String(children).replace(/\n$/, '')}
         language={language}
-        style={oneDark}
+        style={prefersDark ? oneDark : oneLight}
         wrapLongLines={false}
         className={cn(
           '!bg-[unset] !mb-0 max-w-[calc(100vw_-_4rem)] [&>code]:!bg-[unset]',
