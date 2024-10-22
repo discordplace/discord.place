52c916a17a715f45035678d7b6e73c9d5d21d7f2
chimpdev <skyhancloud@gmail.com>
2024-10-23 01:28:22 +0300

    style: update commitlint.config.js to include additional subject-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'always',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case'],
+    ]
+  }
 };
