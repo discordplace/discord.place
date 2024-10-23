a7edf3acca2feacab18746832b3603cd8c5fb575
chimpdev <skyhancloud@gmail.com>
2024-10-23 15:19:27 +0300

    refactor(commitlint): simplify configuration by removing subject-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [
-      2,
-      'always',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case'],
-    ]
-  }
+  extends: ['@commitlint/config-conventional']
 };
