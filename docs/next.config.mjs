18f346b8a4b84255bde4b827d9163d9760ee4988
chimpdev <skyhancloud@gmail.com>
2024-11-08 16:45:15 +0300

    style: remove unnecessary whitespace in next.config.mjs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,6 +1,6 @@
 import remarkGfm from 'remark-gfm';
 import createMDX from '@next/mdx';
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
@@ -12,12 +12,12 @@ const nextConfig = {
     mdxRs: true
   }
 };
- 
+
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: []
   }
 });
- 
+
 export default withMDX(nextConfig);
