7e1643466f64b7761a31fcdd59044fb86c945485
chimpdev <skyhancloud@gmail.com>
2024-12-01 04:09:13 +0300

    fix: update stroke-width to strokeWidth in CollapseIcon components

--- client/app/(account)/account/components/Content/Sidebar/Icons/Collapse.jsx
+++ client/app/(account)/account/components/Content/Sidebar/Icons/Collapse.jsx
@@ -3,7 +3,7 @@ export default function CollapseIcon({ ...props }) {
     <svg
       stroke='currentColor'
       fill='currentColor'
-      stroke-width='0'
+      strokeWidth='0'
       viewBox='0 0 24 24'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
