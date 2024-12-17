53b450f7f1aea2e7dc653135f72bdc7d79b32a69
chimpdev <skyhancloud@gmail.com>
2024-12-17 04:42:42 +0300

    feat: add snowfall effect to LayoutContent

--- client/app/layout-content.js
+++ client/app/layout-content.js
@@ -16,6 +16,7 @@ import useGeneralStore from '@/stores/general';
 import FullPageLoading from './components/FullPageLoading';
 import useLanguageStore from '@/stores/language';
 import ReportButtonProvider from '@/app/components/Providers/ReportButton';
+import Snowfall from 'react-snowfall';
 
 export default function RootLayoutContent({ children }) {
   const language = useLanguageStore(state => state.language);
@@ -30,6 +31,17 @@ export default function RootLayoutContent({ children }) {
 
   return (
     <section key={language}>
+      <Snowfall
+        snowflakeCount={100}
+        speed={[0.5, 1]}
+        wind={[1, 4.5]}
+        radius={[0.5, 3]}
+        style={{
+          zIndex: 9999999,
+          position: 'fixed'
+        }}
+      />
+
       <Script
         defer={true}
         src={config.analytics.script}
