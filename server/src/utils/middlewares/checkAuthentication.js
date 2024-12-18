49994f71d0f525a9ad4b1fdd10d551215ac2d590
chimpdev <skyhancloud@gmail.com>
2024-12-18 05:11:59 +0300

    refactor: simplify authentication middleware by removing redundant token validation

--- server/src/utils/middlewares/checkAuthentication.js
+++ server/src/utils/middlewares/checkAuthentication.js
@@ -1,44 +1,11 @@
-const jwt = require('jsonwebtoken');
-const User = require('@/schemas/User');
-
-class AuthError extends Error {
-  constructor(message) {
-    super(message);
-
-    this.name = 'AuthError';
-  }
-}
-
 module.exports = async function checkAuthentication(request, response, next) {
   const token = request.cookies.token;
 
-  try {
-    if (!token) throw new AuthError('Unauthorized');
+  // We don't need to validate token.
+  // It's already validated by the our custom middleware in server.js
+  // In every request, if there is a token in the cookies, our custom middleware will check it.
+  // So in auth only routes, we just need to check if there is a token in the cookies or not.
+  if (!token) return response.sendError('Token not found.', 401);
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET, {
-      issuer: 'api.discord.place',
-      audience: 'discord.place',
-      subject: 'user'
-    });
-
-    if (!decoded) throw new AuthError('Token invalid.');
-
-    const user = await User.findOne({ id: decoded.id }).select('lastLogoutAt').lean();
-    if (!user) throw new AuthError('User not found.');
-
-    if (decoded.iat < new Date(user.lastLogoutAt).getTime()) throw new AuthError('Token expired.');
-
-    next();
-  } catch (error) {
-    response.clearCookie('token', {
-      httpOnly: true,
-      domain: `.${new URL(config.frontendUrl).hostname}`
-    });
-
-    if (error instanceof AuthError) return response.sendError(error.message, 401);
-
-    logger.error('There was an error while checking authentication:', error);
-
-    return response.sendError('An error occurred while checking authentication.', 500);
-  }
+  next();
 };
