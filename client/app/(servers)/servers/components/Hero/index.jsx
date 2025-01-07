d08e77af8fbef11b8faca52a86c8dc5da1c95f09
chimpdev <skyhancloud@gmail.com>
2025-01-07 07:57:08 +0300

    fix: adjust grid layout for responsive design in Hero component

--- client/app/(servers)/servers/components/Hero/index.jsx
+++ client/app/(servers)/servers/components/Hero/index.jsx
@@ -201,7 +201,7 @@ export default function Hero() {
         ) : (
           <>
             <motion.div
-              className='grid grid-cols-1 gap-4 sm:grid-cols-3'
+              className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
             >
