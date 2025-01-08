1b5127c5e6c9169bbca76852104c7518bea1bba3
chimpdev <skyhancloud@gmail.com>
2025-01-08 09:11:14 +0300

    feat(routes/top-servers): add is_listed field

--- server/src/routes/top-servers.js
+++ server/src/routes/top-servers.js
@@ -1,12 +1,17 @@
+const Server = require('@/schemas/Server');
+
 module.exports = {
-  get: (request, response) => {
+  get: async (request, response) => {
+    const servers = await Server.find({ id: { $in: client.guilds.cache.map(guild => guild.id) } });
+
     const data = client.guilds.cache.map(guild => guild).sort((a, b) => b.memberCount - a.memberCount)
       .slice(0, 5)
       .map(guild => ({
         id: guild.id,
         name: guild.name,
         member_count: guild.memberCount,
-        icon_url: guild.iconURL({ format: 'png', size: 32 })
+        icon_url: guild.iconURL({ format: 'png', size: 32 }),
+        is_listed: servers.some(server => server.id === guild.id)
       }));
 
     return response.json({
