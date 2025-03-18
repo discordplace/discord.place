686aee75f73be210f7476354a0381b3da1b44c0c
chimpdev <skyhancloud@gmail.com>
2025-03-18 04:33:09 +0300

    feat(sounds): add loud sound warning modal

--- client/app/(sounds)/sounds/components/SoundPreview/Waveform.jsx
+++ client/app/(sounds)/sounds/components/SoundPreview/Waveform.jsx
@@ -12,8 +12,10 @@ import { useShallow } from 'zustand/react/shallow';
 import VolumePopover from '@/app/(sounds)/sounds/components/SoundPreview/VolumePopover';
 import config from '@/config';
 import { useLocalStorage } from 'react-use';
+import useModalsStore from '@/stores/modals';
+import { t } from '@/stores/language';
 
-export default function Waveform({ id }) {
+export default function Waveform({ id, name: soundName }) {
   const theme = useThemeStore(state => state.theme);
 
   const { currentlyPlaying, setCurrentlyPlaying, volume, setVolume } = useGeneralStore(useShallow(state => ({
@@ -23,33 +25,89 @@ export default function Waveform({ id }) {
     setVolume: state.sounds.setVolume
   })));
 
+  const { openModal, closeModal } = useModalsStore(useShallow(state => ({
+    openModal: state.openModal,
+    disableButton: state.disableButton,
+    enableButton: state.enableButton,
+    closeModal: state.closeModal,
+    updateModal: state.updateModal
+  })));
+
   const [wavesurfer, setWavesurfer] = useState(null);
   const [currentTime, setCurrentTime] = useState(0);
   const [totalTime, setTotalTime] = useState(0);
   const [localVolume, setLocalVolume] = useLocalStorage('volume', 1);
+  const [isLoud, setIsLoud] = useState(false);
+  const [localLoudSoundsIgnored, setLocalLoudSoundsIgnored] = useLocalStorage('soundplayer_loudSoundsIgnored', false);
+  const loudSoundsIgnored = useGeneralStore(state => state.soundPlayer.loudSoundsIgnored);
+  const setLoudSoundsIgnored = useGeneralStore(state => state.soundPlayer.setLoudSoundsIgnored);
 
-  const onPlayPause = () => {
+  function onPlayPause() {
     if (!wavesurfer) return;
 
     if (currentlyPlaying === id) setCurrentlyPlaying('');
-    else setCurrentlyPlaying(id);
-  };
+    else if (isLoud && !loudSoundsIgnored) {
+      openModal(`sound-${id}-is-loud`, {
+        title: t('soundPlayer.loudWarningModal.title', { soundName }),
+        description: t('soundPlayer.loudWarningModal.description'),
+        content: t('soundPlayer.loudWarningModal.content'),
+        buttons: [
+          {
+            id: 'dont-show-again',
+            label: t('buttons.dontShowAgain'),
+            variant: 'ghost',
+            action: () => {
+              closeModal(`sound-${id}-is-loud`);
+              setLocalLoudSoundsIgnored(true);
+              setCurrentlyPlaying(id);
+            }
+          },
+          {
+            id: 'cancel',
+            label: t('buttons.cancel'),
+            variant: 'outlined',
+            actionType: 'close'
+          },
+          {
+            id: 'continue',
+            label: t('buttons.continue'),
+            variant: 'solid',
+            action: () => {
+              closeModal(`sound-${id}-is-loud`);
+              setCurrentlyPlaying(id);
+            }
+          }
+        ]
+      });
+    } else setCurrentlyPlaying(id);
+  }
 
   useEffect(() => {
     if (!wavesurfer) return;
 
-    wavesurfer.on('audioprocess', time => setCurrentTime(time));
+    function handleAudioProcess(time) {
+      setCurrentTime(time);
+    }
+
+    wavesurfer.on('audioprocess', handleAudioProcess);
     wavesurfer.setVolume(volume);
     setLocalVolume(volume);
 
+    return () => wavesurfer.un('audioprocess', handleAudioProcess);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [volume, wavesurfer]);
 
   useEffect(() => {
     setVolume(localVolume);
+    setLoudSoundsIgnored(localLoudSoundsIgnored);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    setLoudSoundsIgnored(localLoudSoundsIgnored);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [localLoudSoundsIgnored]);
 
   useEffect(() => {
     if (!wavesurfer) return;
@@ -81,6 +139,21 @@ export default function Waveform({ id }) {
         onReady={wavesurfer => {
           setTotalTime(wavesurfer.getDuration());
           setWavesurfer(wavesurfer);
+
+          const decodedData = wavesurfer.decodedData;
+          if (!decodedData) return;
+
+          const channelData = decodedData.getChannelData(0);
+          const frameSize = 1024;
+          let maxAmplitude = 0;
+
+          for (let i = 0; i < channelData.length; i += frameSize) {
+            const chunk = channelData.slice(i, i + frameSize);
+            const peak = Math.max(...chunk);
+            if (peak > maxAmplitude) maxAmplitude = peak;
+          }
+
+          setIsLoud(maxAmplitude > 0.7);
         }}
         onSeeking={wavesurfer => setCurrentTime(wavesurfer.getCurrentTime())}
       />
