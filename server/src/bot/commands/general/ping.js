cba25e8fa36a0b77aa784d1482ad08bf9c163356
chimpdev <skyhancloud@gmail.com>
2025-04-11 19:48:11 +0300

    fix(ping): remove extraneous character in response content

--- server/src/bot/commands/general/ping.js
+++ server/src/bot/commands/general/ping.js
@@ -36,7 +36,7 @@ module.exports = {
       content: `\`\`\`ansi
 ${ansiColors.bold(`${ansiColors.blue(await interaction.translate('commands.ping.fields.0.name'))} ${websocketHeartbeat}ms
 ${ansiColors.reset(await interaction.translate('commands.ping.fields.0.value'))}
-z
+
 ${ansiColors.blue(await interaction.translate('commands.ping.fields.1.name'))} ${serverResponseTime}ms
 ${ansiColors.reset(await interaction.translate('commands.ping.fields.1.value'))}
 
