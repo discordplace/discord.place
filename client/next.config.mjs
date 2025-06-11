1ad83fdf50d25755e975938fbe94fa99fcced012
Gökhan Bulut <root@skyhan.cloud>
2025-06-11 06:34:15 +0300

    fix(client): handle correctly remotePatterns URL errors in next.config file

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,19 +3,29 @@ const withBundleAnalyzer = pgk({ enabled: process.env.ANALYZE === 'true' });
 
 /** @type {import('next').NextConfig} */
 
+const remotePatterns = [
+  {
+    protocol: 'https',
+    hostname: 'cdn.discordapp.com'
+  }
+];
+
+if (process.env.NEXT_PUBLIC_CDN_URL) {
+  try {
+    const cdnUrl = new URL(process.env.NEXT_PUBLIC_CDN_URL);
+    remotePatterns.push({
+      protocol: cdnUrl.protocol.replace(':', ''),
+      hostname: cdnUrl.hostname
+    });
+  } catch (error) {
+    console.error('Invalid NEXT_PUBLIC_CDN_URL. Please check your environment variable.');
+  }
+}
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com'
-      },
-      {
-        protocol: 'https',
-        hostname: new URL(process.env.NEXT_PUBLIC_CDN_URL).hostname
-      }
-    ]
+    remotePatterns
   },
   async rewrites() {
     return [
