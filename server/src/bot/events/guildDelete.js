97eda534a1e892afc86fe4aef82caa50ecfb8413
Gökhan Bulut <root@skyhan.cloud>
2025-08-25 13:51:40 +0300

    fix: remove logging from guildDelete event

--- server/src/bot/events/guildDelete.js
+++ server/src/bot/events/guildDelete.js
@@ -1,7 +1,6 @@
 const updateClientActivity = require('@/utils/updateClientActivity');
 const Review = require('@/schemas/Server/Review');
 const Reward = require('@/schemas/Server/Vote/Reward');
-const sendLog = require('@/utils/sendLog');
 
 module.exports = async guild => {
   logger.info(`Kicked from guild ${guild.name} (${guild.id}).`);
@@ -12,16 +11,4 @@ module.exports = async guild => {
   ]);
 
   updateClientActivity();
-
-  sendLog(
-    'leavedGuild',
-    [
-      { type: 'guild', name: 'Guild', value: guild },
-      { type: 'number', name: 'Members', value: guild.memberCount },
-      { type: 'owner', name: 'Owner', value: guild.ownerId }
-    ],
-    [
-      { label: 'View Owner', url: `${config.frontendUrl}/profile/u/${guild.ownerId}` }
-    ]
-  );
 };
