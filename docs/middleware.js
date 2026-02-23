ee81354cd7e680b1a6cbaed77f6eae8f97051709
Gökhan Bulut <root@skyhan.cloud>
2026-02-23 20:06:12 +0300

    feat: remove no more used /docs

--- docs/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  return NextResponse.redirect(new URL('/home', request.nextUrl));
-}
-
-export const config = {
-  matcher: '/'
-};
--- docs/utils/cn.js
+++
@@ -1,6 +0,0 @@
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export default function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
